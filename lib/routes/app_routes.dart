5dbbf2d2388cbae9a3897115caf5688edfe4416b
Valentyn Riabinchak <rabincakv@gmail.com>
2026-03-30 17:14:44 +0300

    Lab3

--- lib/routes/app_routes.dart
+++
@@ -1,8 +0,0 @@
-abstract class AppRoutes {
-  static const String splash = '/';
-  static const String register = '/register';
-  static const String home = '/home';
-  static const String details = '/details';
-  static const String contacts = '/contacts';
-  static const String role = '/role';
-}
